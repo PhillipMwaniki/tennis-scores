14539ea3bb78e2392cf9bbcbafc635081ce5c7e4
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-04-21 13:56:17 +0300

    cleaned up dataset

--- tests/Unit/TennisTest.php
+++ tests/Unit/TennisTest.php
@@ -9,16 +9,16 @@ test('returns the correct score', function ($player1, $player2, $score) {
     expect($testScore)->toBe($score);
 })->with([
     [0, 0, 'Love All'],
+    [0, 4, 'Winner: Player 2'],
     [1, 0, 'Fifteen-Love'],
     [1, 1, 'Fifteen All'],
     [2, 0, 'Thirty-Love'],
-    [3, 0, 'Forty-Love'],
     [2, 2, 'Thirty All'],
+    [3, 0, 'Forty-Love'],
     [3, 3, 'Deuce'],
-    [4, 4, 'Deuce'],
-    [5, 5, 'Deuce'],
-    [4, 3, 'Advantage: Player 1'],
     [3, 4, 'Advantage: Player 2'],
     [4, 0, 'Winner: Player 1'],
-    [0, 4, 'Winner: Player 2'],
+    [4, 3, 'Advantage: Player 1'],
+    [4, 4, 'Deuce'],
+    [5, 5, 'Deuce'],
 ]);
