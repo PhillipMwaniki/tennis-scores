2b82d587bab94ac7da503390f92d3b849e725ee1
Chris Muller <chris@salthq.co>
2022-04-21 14:49:39 +0200

    Add additional validation for impossible tennis scores

--- app/Http/Requests/GameRequest.php
+++ app/Http/Requests/GameRequest.php
@@ -2,15 +2,27 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class GameRequest extends FormRequest
 {
     public function rules()
     {
+        $player1 = $this->player1;
+        $player2 = $this->player2;
+
         return [
-            'player1' => ['min:0', 'required', 'integer', 'numeric'],
-            'player2' => ['min:0', 'required', 'integer', 'numeric'],
+            'player1' => ['min:0', 'required', 'integer', 'numeric', Rule::prohibitedIf(abs($player1-$player2) > 2 && ($player1 > 4 || $player2 > 4))],
+            'player2' => ['min:0', 'required', 'integer', 'numeric', Rule::prohibitedIf(abs($player1-$player2) > 2 && ($player1 > 4 || $player2 > 4))],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+                'player1.prohibited' => 'This isn\'t a possible tennis score',
+                'player2.prohibited' => 'This isn\'t a possible tennis score',
         ];
     }
 }
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,32 +2,37 @@
     <div>
         <h1>Tennis Scores</h1>
         <form method="post" @submit.prevent="getScores">
-            <input type="number" v-model="form.player1" min="0">
-            <input type="number" v-model="form.player2" min="0">
+            <div>
+                <input type="number" v-model="form.player1" min="0" />
+                <div v-if="form.errors.player1">{{ form.errors.player1 }}</div>
+            </div>
+            <div>
+                <input type="number" v-model="form.player2" min="0" />
+                <div v-if="form.errors.player2">{{ form.errors.player2 }}</div>
+            </div>
             <button type="submit">Get Scores</button>
+            <div v-if="form.errors.score">{{ form.errors.score }}</div>
         </form>
         <h4 v-if="$page.props.flash.score">Score</h4>
         <h2>{{ $page.props.flash.score }}</h2>
     </div>
 </template>
 
-
 <script>
-    export default {
-        data() {
-            return {
-                form: {
-                    player1: 0,
-                    player2: 0,
-                },
-            }
+export default {
+    data() {
+        return {
+            form: this.$inertia.form({
+                player1: 0,
+                player2: 0,
+            }),
+        };
+    },
+    methods: {
+        getScores() {
+            console.log("post action");
+            this.form.post("/scores");
         },
-        methods: {
-            getScores() {
-                console.log('post action');
-                this.$inertia.post('/scores', this.form);
-            }
-        }
-
-    }
+    },
+};
 </script>
